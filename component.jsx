71a5cc3c4a543cdc8bb20b00b84acfa599f5be7d
Mahendra Singh <mahendra.life@hotmail.com>
2022-02-16 13:10:24 +0530

    Update component.jsx

--- component.jsx
+++ component.jsx
@@ -3,6 +3,6 @@ import { useTranslation } from "react-i18next";
 const Home = () => {
   const title ="welcome";
   const { translate } = useTranslation();
-  return (<div> {t(title)}</div>)
+  return (<div> {translate(title)}</div>)
 
 };
